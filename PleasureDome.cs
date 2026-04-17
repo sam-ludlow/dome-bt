fb994b6228fb6e9168b41407513cb630f93fb5b8
sam-ludlow <sam@spludlow.co.uk>
2026-04-17 11:08:00 +0100

    Handle encrypted magnets

--- PleasureDome.cs
+++ PleasureDome.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
 
 using HtmlAgilityPack;
+using Newtonsoft.Json;
 
 namespace dome_bt
 {
 	public class PleasureDome
 	{
+		private static string MagnetKey = "RRt08v+YWc2+910RGOhZO7DrNVnHKae8MDJyJNOd950=";
+
 		public static void ParseMagentLinks()
 		{
 			Tools.ConsoleHeading(1, new string[] { "Magnet Scrape" });
 
 			if (Globals.Cores.Contains("mame"))
-				ParseMagentLinks("https://data.spludlow.co.uk/magnets/mame.html",
+				ParseMagentLinks("https://data.spludlow.co.uk/api/magnets/mame",
 					new AssetType[] { AssetType.MachineRom, AssetType.MachineDisk, AssetType.SoftwareRom, AssetType.SoftwareDisk },
 					new List<string>(new string[] { "ROMs (merged)", "CHDs (merged)", "Software List ROMs (merged)", "Software List CHDs (merged)" }),
 					Globals.Magnets);
 
 			if (Globals.Cores.Contains("hbmame"))
-				ParseMagentLinks("https://data.spludlow.co.uk/magnets/hbmame.html",
+				ParseMagentLinks("https://data.spludlow.co.uk/api/magnets/hbmame",
 					new AssetType[] { AssetType.HbMameMachineRom, AssetType.HbMameSoftwareRom, },
 					new List<string>(new string[] { "ROMs (merged)", "Software List ROMs (merged)", }),
 					Globals.Magnets);
@@ -28,7 +33,22 @@ namespace dome_bt
 		{
 			Console.WriteLine(url);
 
-			string html = Tools.FetchCached(url) ?? throw new ApplicationException("Can't fetch HTML");
+			dynamic json = JsonConvert.DeserializeObject<dynamic>(Tools.FetchCached(url) ?? throw new ApplicationException("Can't fetch Magnets"));
+
+			string html;
+			using (var aes = Aes.Create())
+			{
+				aes.Key = Convert.FromBase64String(MagnetKey);
+				aes.IV = Convert.FromBase64String((string)json.iv);
+				aes.Mode = CipherMode.CBC;
+				aes.Padding = PaddingMode.PKCS7;
+
+				using (var decryptor = aes.CreateDecryptor())
+					using (var stream = new MemoryStream(Convert.FromBase64String((string)json.body)))
+						using (var cryStream = new CryptoStream(stream, decryptor, CryptoStreamMode.Read))
+							using (var reader = new StreamReader(cryStream))
+								html = reader.ReadToEnd();
+			}
 
 			HtmlDocument doc = new HtmlDocument();
 			doc.LoadHtml(html);
